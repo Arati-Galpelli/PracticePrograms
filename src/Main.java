41c115b40e63c4842e3e580bd7674c79f825fa63
“AratiG“ <aratinalla@gmail.com>
2023-05-10 13:19:27 +0530

    third commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        public static void main(String[] args) {
             System.out.println("My first name is : "+args[0]);
             System.out.println("My Middle name is : "+args[1]);
             System.out.println("My Last name is : "+args[2]);
