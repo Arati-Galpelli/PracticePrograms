c630b49b978e8e6e13c9dafedecdf0bccf23b6fb
“AratiG“ <aratinalla@gmail.com>
2023-05-10 12:50:12 +0530

    file edited2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-
+            display();
+        }
+        static void display()
+        {
+            System.out.println("HelloWorld...");
         }
     }
-}
