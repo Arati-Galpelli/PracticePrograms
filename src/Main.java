81606a3cad139ef5d88591b9ac587ad0835d58fb
“AratiG“ <aratinalla@gmail.com>
2023-05-10 12:55:37 +0530

    file edited3

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-            display();
-        }
-        static void display()
-        {
-            System.out.println("HelloWorld...");
+        static int a;
+        static float b;
+        static double c;
+        static long d;
+        static String e;
+        static boolean f;
+        public static void main (String[]args){
+            System.out.println("default value of int = " + a);
+            System.out.println("default value of float  = " + b);
+            System.out.println("default value of double  = " + c);
+            System.out.println("default value of long  = " + d);
+            System.out.println("default value of string  = " + e);
+            System.out.println("default value of boolean  = " + f);
         }
     }
