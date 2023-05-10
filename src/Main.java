c56a95f0116bd43c4c036fb1bd9a68946856d340
“AratiG“ <aratinalla@gmail.com>
2023-05-10 13:28:36 +0530

    file commit3

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,24 @@
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-            System.out.println("My first name is : "+args[0]);
-            System.out.println("My Middle name is : "+args[1]);
-            System.out.println("My Last name is : "+args[2]);
-            System.out.println("My name is : "+args[0]+" "+args[1]+" "+args[2]);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter an Year :");
+        int year = sc.nextInt();
+        int res = isLeap(year);
+        if(year>=1585) {
+            if (res == 1)
+                System.out.println("Given year is leap year");
+            else
+                System.out.println("Given year is not leap year");
         }
+        else
+            System.out.println("Invalid Year");
+
     }
+    static int isLeap(int y) {
+        if (((y % 4 == 0) && (y % 100 != 0)) || (y % 400 == 0))
+            return 1;
+        else
+            return 0;
+    }
+}
